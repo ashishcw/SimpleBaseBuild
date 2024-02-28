777adb7ddf106e25915e77c1318ab5ba920cd28b
ashishcw <59785028+ashishcw@users.noreply.github.com>
2024-02-28 16:38:04 +0530

    Base Template Engine
    1. Base Template Engine
    2. Added main loop and thread handling

---
+++ src/main/java/org/ashish/basebuild/constant/Constants.java
@@ -0,0 +1,18 @@
+package org.ashish.basebuild.constant;
+
+public class Constants {
+    //Main
+    public static final double VERSION = 1.0;
+
+    //Display settings
+    public static final int WINDOW_WIDTH = 800;
+    public static final int WINDOW_HEIGHT = 800;
+    public static final int FPS = 60;
+    public static final long OPTIMAL_TIME = 1000000000 / FPS;
+
+    //Node settings
+    public static final int NODE_SIZE = 16;
+    public static final int NODE_OFFSET = 16;
+    public static final int MAX_ROWS = 15;
+    public static final int MAX_COLS = 15;
+}
