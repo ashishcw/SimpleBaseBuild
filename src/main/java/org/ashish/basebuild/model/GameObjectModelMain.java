15d864d8e72db7eeb2767bf7e3ae146b354dac89
ashishcw <59785028+ashishcw@users.noreply.github.com>
2024-02-28 17:40:37 +0530

    #2-Base-GameObject-Mode and Handler
    1. Game object mode added
    2. Handler class added
    3. Temp object Class added(This is a temporary class to test the core functionality)

---
+++ src/main/java/org/ashish/basebuild/model/GameObjectModelMain.java
@@ -0,0 +1,116 @@
+package org.ashish.basebuild.model;
+
+import java.awt.*;
+import java.util.UUID;
+
+public abstract class GameObjectModelMain {
+    private UUID id;
+    private int xPos, yPos;
+    private int sizeWidth, sizeHeight;
+    private float xVel, yVel;
+    private int row, col;
+    private Color color;
+    private String name;
+    private boolean isActiveinScene;
+
+    public GameObjectModelMain(int xPos, int yPos) {
+        this.id = UUID.randomUUID();
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public abstract void tick();
+    public abstract void render(Graphics g);
+
+    public UUID getId() {
+        return id;
+    }
+
+    public int getxPos() {
+        return xPos;
+    }
+
+    public void setxPos(int xPos) {
+        this.xPos = xPos;
+    }
+
+    public int getyPos() {
+        return yPos;
+    }
+
+    public void setyPos(int yPos) {
+        this.yPos = yPos;
+    }
+
+    public int getSizeWidth() {
+        return sizeWidth;
+    }
+
+    public void setSizeWidth(int sizeWidth) {
+        this.sizeWidth = sizeWidth;
+    }
+
+    public int getSizeHeight() {
+        return sizeHeight;
+    }
+
+    public void setSizeHeight(int sizeHeight) {
+        this.sizeHeight = sizeHeight;
+    }
+
+    public float getxVel() {
+        return xVel;
+    }
+
+    public void setxVel(float xVel) {
+        this.xVel = xVel;
+    }
+
+    public float getyVel() {
+        return yVel;
+    }
+
+    public void setyVel(float yVel) {
+        this.yVel = yVel;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActiveinScene() {
+        return isActiveinScene;
+    }
+
+    public void setActiveinScene(boolean activeinScene) {
+        isActiveinScene = activeinScene;
+    }
+}
